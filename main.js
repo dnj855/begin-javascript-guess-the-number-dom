44945eee0a192af9de395446b6f2be7c7d76cb28
Cédric Lang-Roth <contact@clangroth.fr>
2024-05-04 16:44:00 +0200

    Little design enhancement

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 import "./style.css";
+
+class Game {
+  constructor() {}
+
+  init() {
+    this.startButton = document.querySelector("#start-button");
+    this.startDiv = document.querySelector("#start-game");
+    this.gameDiv = document.querySelector("#game");
+    this.startSequence();
+  }
+
+  startSequence() {
+    this.startButton?.addEventListener("click", () => {
+      this.startDiv?.classList.toggle("hidden");
+      this.gameDiv?.classList.toggle("hidden");
+    });
+  }
+}
+
+const game = new Game();
+game.init();
