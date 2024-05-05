c9885926613e0d3c5770ca6ee186678e8df56971
Cédric Lang-Roth <contact@clangroth.fr>
2024-05-05 22:50:59 +0200

    chore: Refactor game initialization and add number range display

--- main.js
+++ main.js
@@ -1,20 +1,38 @@
 import "./style.css";
 
 class Game {
-  constructor() {}
+  static MIN_NUMBER = 0;
+  static MAX_NUMBER = 500;
+
+  constructor() {
+    this.score = 0;
+  }
 
   init() {
+    this.number =
+      Math.floor(Math.random() - Game.MIN_NUMBER + 1) * Game.MAX_NUMBER;
+    this.minNumberDisplays = document.querySelectorAll(".min-number");
+    this.maxNumberDisplays = document.querySelectorAll(".max-number");
     this.startButton = document.querySelector("#start-button");
     this.startDiv = document.querySelector("#start-game");
     this.gameDiv = document.querySelector("#game");
-    this.startSequence();
+    this.form = document.querySelector("form");
+    this.scoreDisplay = document.querySelector("#score");
+    this.resultDisplay = document.querySelector("#result");
+    this.startButton?.addEventListener("click", () => {
+      this.showGame();
+    });
   }
 
-  startSequence() {
-    this.startButton?.addEventListener("click", () => {
-      this.startDiv?.classList.toggle("hidden");
-      this.gameDiv?.classList.toggle("hidden");
+  showGame() {
+    this.startDiv?.classList.toggle("hidden");
+    this.minNumberDisplays.forEach((display) => {
+      display.textContent = Game.MIN_NUMBER;
     });
+    this.maxNumberDisplays.forEach((display) => {
+      display.textContent = Game.MAX_NUMBER;
+    });
+    this.gameDiv?.classList.toggle("hidden");
   }
 }
 
